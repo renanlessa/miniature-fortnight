e6359399d1b7f7c949b52d489cc1a5350bc17b7b
Renan Lessa <renanlessa@gmail.com>
2024-10-17 07:39:15 -0300

    refact: alterado nome metodo que buscar usuario por email

--- src/main/java/dev/flix/repository/UserRepository.java
+++ src/main/java/dev/flix/repository/UserRepository.java
@@ -4,11 +4,7 @@ import dev.flix.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
-
-    UserDetails findUserByEmail(String email);
+    UserDetails findByEmail(String email);
 }
