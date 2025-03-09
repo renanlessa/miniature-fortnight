a6c4f8e36588419db563cfd630187dda023eed64
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2025-03-09 04:36:13 -0300

    validate token com head

--- src/main/java/dev/flix/config/SecurityConfig.java
+++ src/main/java/dev/flix/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.HEAD, "/auth/validate").authenticated()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
